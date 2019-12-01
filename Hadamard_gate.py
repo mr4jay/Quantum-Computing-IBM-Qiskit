5ca155f2a15a90ba57867988b5f0c89719842224
Pasam Revanth kumar <33693651+pasamrevanthkumar1998@users.noreply.github.com>
2019-12-01 20:09:33 +0530

    Add files via upload

---
+++ Hadamard_gate.py
@@ -0,0 +1,15 @@
+from qiskit import QuantumRegister,QuantumCircuit,execute,Aer
+from qiskit import IBMQ
+IBMQ.load_account()
+S_simulator=Aer.backends(name='statevector_simulator')[0]
+M_simulator=Aer.backends(name='qasm_simulator')[0]
+
+q=QuantumRegister(1)
+hello_qubit=QuantumCircuit(q)
+
+hello_qubit.h(q[0])
+
+
+job=execute(hello_qubit,S_simulator)
+result=job.result()
+result.get_statevector()
