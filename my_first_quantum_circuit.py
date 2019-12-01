5da92ad414c31f8b4c5fea9f081f9bb06858d30a
Pasam Revanth kumar <33693651+pasamrevanthkumar1998@users.noreply.github.com>
2019-12-01 10:02:27 +0530

    Add files via upload

---
+++ my_first_quantum_circuit.py
@@ -0,0 +1,15 @@
+import qiskit
+from qiskit import ClassicalRegister,QuantumRegister,QuantumCircuit,execute,Aer
+from qiskit import IBMQ
+IBMQ.load_account()
+S_simulator=Aer.backends(name='statevector_simulator')[0]
+M_simulator=Aer.backends(name='qasm_simulator')[0]
+
+q=QuantumRegister(1)
+hello_qubit=QuantumCircuit(q)
+
+hello_qubit.iden(q)
+
+job=execute(hello_qubit,S_simulator)
+result=job.result()
+result.get_statevector()
