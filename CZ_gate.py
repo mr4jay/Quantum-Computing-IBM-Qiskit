ead1f69da598b65479c67975722024eb5fd6a0aa
Pasam Revanth kumar <33693651+pasamrevanthkumar1998@users.noreply.github.com>
2019-12-05 05:06:06 +0530

    Add files via upload

---
+++ CZ_gate.py
@@ -0,0 +1,16 @@
+from qiskit import ClassicalRegister,QuantumRegister,QuantumCircuit,Aer
+from qiskit.tools.visualization import circuit_drawer
+from qiskit import IBMQ
+IBMQ.load_account()
+S_simulator=Aer.backends(name='statevector_simulator')[0]
+M_simulator=Aer.backends(name='qasm_simulator')[0]
+
+q=QuantumRegister(2,name='q')
+c=ClassicalRegister(2,name='c')
+qc=QuantumCircuit(q,c,name='qc')
+
+qc.iden(q[0])
+qc.iden(q[1])
+qc.cz(q[0],q[1])
+
+print(circuit_drawer(qc))
